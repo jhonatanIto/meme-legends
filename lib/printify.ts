78d7be095b4bb7e202950ac31fd334c7bd86d388
Jhonatan <jhonatan-ito@hotmail.com>
2026-04-24 15:01:06 +0900

    printify address

--- lib/printify.ts
+++ lib/printify.ts
@@ -112,11 +112,7 @@ export const createPrintifyOrder = async ({
           city: shipping?.address?.city || "",
           address1: shipping?.address?.line1 || "",
           address2: shipping?.address?.line2 || "",
-          zip:
-            shipping?.address?.postal_code?.replace(
-              /^(\d{3})(\d{4})$/,
-              "$1-$2",
-            ) || "",
+          zip: shipping?.address?.postal_code,
           phone: shipping?.phone || "",
         },
       }),
